a2b5b6db769452849ccf23ad73af8396c333f493
周顺 <758678856@qq.com>
2022-12-15 22:56:44 +0800

    代码优化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup name="App">
+	const route = useRoute()
+	const router = useRouter()
+	// console.log(route, router)
 	
+	const tranName = ref('')
+
+	watch(() => router.currentRoute.value, (to, from) => {
+		console.log(to, from)
+		const names = ['index', 'box', 'my']
+		if (from.path == '/' || (names.includes(to.name) && names.includes(from.name))) {
+			tranName.value = ''
+		} else {
+			tranName.value = 'pop-in-right'
+		}
+	}, { immediate: false })
 </script>
 
 <template>
 	<Suspense>
 		<router-view v-slot="{ Component, route }">
-			<transition name="fold-left">
+			<transition :name="tranName">
 				<keep-alive>
 					<component
 						:is="Component"
