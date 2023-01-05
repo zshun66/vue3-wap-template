c7f49ce6d89b1d42520841ed0f19542b3a40aebd
zhoushun <229591451@qq.com>
2023-01-05 18:49:44 +0800

    自定义图片懒加载指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,7 +1,11 @@
 import focus from './modules/v-focus.js'
+import lazy from './modules/v-focus.js'
+import emoji from './modules/v-emoji.js'
 
 export default {
 	install(app) {
 		app.directive('focus', focus)
+		app.directive('lazy', lazy)
+		app.directive('emoji', emoji)
 	}
 }
